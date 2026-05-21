89c69306a50ce67c5b700c9f0027b61a8706a6f0
sandeep <sandeepmishra898k@gmail.com>
2026-05-21 17:33:52 +0530

    UC 4 – Solving Using Switch Case

--- App/src/EmployeeWageComputation.java
+++ App/src/EmployeeWageComputation.java
@@ -6,16 +6,23 @@ public class EmployeeWageComputation {
 
     public static void main(String[] args) {
 
-        int empHrs = 0;
+        int empHrs;
 
         int empCheck = (int) (Math.random() * 3);
 
-        if (empCheck == IS_PART_TIME)
-            empHrs = 4;
-        else if (empCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else
-            empHrs = 0;
+        switch (empCheck) {
+
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+
+            default:
+                empHrs = 0;
+        }
 
         int empWage = empHrs * EMP_RATE_PER_HOUR;
 
