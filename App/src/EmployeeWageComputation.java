95f5ea47f02f96165a0e5a790deccc7a0e3e98d5
sandeep <sandeepmishra898k@gmail.com>
2026-05-21 17:38:36 +0530

    UC 6 – Calculate Wage Till Condition Reached

--- App/src/EmployeeWageComputation.java
+++ App/src/EmployeeWageComputation.java
@@ -1,13 +1,41 @@
 public class EmployeeWageComputation {
 
     public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int WORKING_DAYS = 20;
-    public static final int FULL_DAY_HOUR = 8;
+    public static final int MAX_HOURS = 100;
+    public static final int MAX_DAYS = 20;
 
     public static void main(String[] args) {
 
-        int totalWage = WORKING_DAYS * FULL_DAY_HOUR * EMP_RATE_PER_HOUR;
+        int totalHours = 0;
+        int totalDays = 0;
 
-        System.out.println("Monthly Wage : " + totalWage);
+        while (totalHours < MAX_HOURS && totalDays < MAX_DAYS) {
+
+            totalDays++;
+
+            int empCheck = (int) (Math.random() * 3);
+
+            int empHrs;
+
+            switch (empCheck) {
+
+                case 1:
+                    empHrs = 4;
+                    break;
+
+                case 2:
+                    empHrs = 8;
+                    break;
+
+                default:
+                    empHrs = 0;
+            }
+
+            totalHours += empHrs;
+        }
+
+        int totalWage = totalHours * EMP_RATE_PER_HOUR;
+
+        System.out.println("Total Wage : " + totalWage);
     }
 }
